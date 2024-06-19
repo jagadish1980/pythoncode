4807ea2b20542a0cb9ce146f410d7870687bbc3e
sukanya.ravinder@gmail.com <sukanya.ravinder@gmail.com>
2024-06-19 15:01:35 -0400

    Raju commit JIRA19JUNE2024

---
+++ Dimensional 2nd.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Creating array & testing it
+
+# Creating a 1D array
+array_1d = np.array([1, 2, 3, 4, 5])
+
+# Creating a 2D array
+array_2d = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Creating a 3D array
+array_3d = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+print("1D array:\n", array_1d)
+print("2D array:\n", array_2d)
+print("3D array:\n", array_3d)
