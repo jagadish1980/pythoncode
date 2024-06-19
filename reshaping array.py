cad00c0185bd87124cb828bc6c2685140a4986e4
sukanya.ravinder@gmail.com <sukanya.ravinder@gmail.com>
2024-06-19 14:52:46 -0400

    raju commit

--- reshaping array.py
+++ reshaping array.py
@@ -3,7 +3,6 @@ import numpy as np
 
 # Creating a 2D array
 array_2rd = np.array([[1, 2, 3, 4, 5], [6, 7, 8,9,10],[11,12,13,14,15]])
-
 print("2nd dimension")
 print("2rd array","\n",array_2rd)
 print("array_2d , NDim #", array_2rd.ndim)
